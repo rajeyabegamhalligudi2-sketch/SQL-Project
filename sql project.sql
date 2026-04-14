09fe8fcfa2a687f583d5292b1a0a9be3527a12ae
Rajeyabegam Halligudi <rajeyabegamhalligudi2@gmail.com>
2026-04-14 20:13:17 +0530

    Add files via upload

---
+++ sql project.sql
@@ -0,0 +1,10 @@
+INSERT INTO customers6 (
+    customer_id, cust_first_name, customer_last_name,
+    salary, department, payment_date, role_in_the_company
+)
+VALUES (
+    1011, 'Arjun', 'Rao',
+    75000, 'IT', '2026-04-01', 'Engineer'
+);
+
+SELECT * FROM customers;
